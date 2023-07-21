ccb8e1f05584492bc81f861cc8f45e7df64ac679
4017 Сгадов И.В <sgadov2vanya@yandex.ru>
2023-07-21 20:48:22 +0300

    58. Length of Last Word

---
+++ 58. Length of Last Word.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  const length = s.length;
+  let wasL = false;
+  let count = 0;
+  for (let i = length - 1; i >= 0; i--) {
+    const elem = s[i];
+    if (elem != ' ') {
+      wasL = true;
+      count += 1;
+    } else {
+      if (wasL) {
+        break;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(lengthOfLastWord('a '));
